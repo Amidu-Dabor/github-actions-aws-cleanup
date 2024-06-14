f6d5b6d127e1fa968f29acebde019d31e622b48a
Amidu Dabor <36164952+Amidu-Dabor@users.noreply.github.com>
2024-06-14 19:39:15 +0300

    Update aws-cleanup-for-all-resources.ps1

--- aws-cleanup-for-all-resources.ps1
+++ aws-cleanup-for-all-resources.ps1
@@ -3,10 +3,10 @@ param (
 )
 
 # Initialize lists outside the param block
-$StateMachineList = aws stepfunctions list-state-machines | ConvertFrom-Json
-$ActivityList = aws stepfunctions list-activities | ConvertFrom-Json
-$RegionList = (aws ec2 describe-regions | ConvertFrom-Json).Regions | Where-Object { $_.RegionName -match 'us-east|us-west' }
-$ec2InstanceList = aws ec2 describe-instances | ConvertFrom-Json
+$StateMachineList = aws stepfunctions list-state-machines # | ConvertFrom-Json
+$ActivityList = aws stepfunctions list-activities # | ConvertFrom-Json
+$RegionList = (aws ec2 describe-regions).Regions | Where-Object { $_.RegionName -match 'us-east|us-west' }
+$ec2InstanceList = aws ec2 describe-instances # | ConvertFrom-Json
 
 function Cleanup-AWS-Resources-If-Exist {
     param (
